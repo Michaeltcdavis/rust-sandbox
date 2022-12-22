b3f067fcd314a296df10f85f7692f9be1c6d3a02
Michael Davis <michaeltcdavis@gmail.com>
2022-12-22 18:06:40 -0500

    test some arrays

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let numbers = [1, 2, 3];
- 
-let nums: Vec<i32> = numbers.iter()
-  .map(|x| x * 2 )
-  .collect();
- 
-println!("{nums:?}"); // [2, 4, 6]
+    let array = [1,2,3];
+    let array2 = [1; 3];
+    for n in array {
+      println!("{n}")
+    }
 }
