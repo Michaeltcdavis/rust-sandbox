0019b69614f3f49e19c4ba903101e5298dfad9fb
Michael Davis <michaeltcdavis@gmail.com>
2022-12-22 19:06:00 -0500

    more array tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let array = [1,2,3];
-    let array2 = [1; 3];
+    let array2 = ['c'; 3];
     for n in array {
       println!("{n}")
     }
+    array2.iter().map(|c| println!("{c}"));
 }
